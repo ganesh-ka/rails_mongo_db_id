0b8f47c325fbf93d860591ce571c44bdebf4a354
ganesh <ganesh@ganesh-at-artha42.(none)>
2010-10-22 12:16:10 +0530

    Initial commit of unmodified new Rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailsMongoDbId::Application.config.secret_token = '0576e78de1258058c805ca22b8674835587947ea363dcd276ff80d568a781c31e7fec0f4dd1a8844bb15c8d8e1bde8bb5335de9e4838c1fdc7106904ea0d3da7'
